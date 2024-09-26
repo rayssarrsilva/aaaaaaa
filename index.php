6d46b1b80d5ce7cb8e41e2e6175e16f21bbfad02
rayssa <116031703+rayssarrsilva@users.noreply.github.com>
2024-09-26 17:33:05 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <title>titulo</title>
+  <head>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <form method="post" action="login.php" style="text-align: center; width: 49vw; margin-left : 25vw;">
+      <div>CPF:<input type="text" name="cpf"><br></div>
+      <div>SENHA: <input type="password" name="senha"><br></div>
+      <input type="submit" value="Enviar">
+    </form>
+
+  </body>
+</html>
